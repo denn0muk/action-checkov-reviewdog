85f755285d403b14efc599bbfd19e03f6415c5c1
misterek <misterek@gmail.com>
2023-07-20 18:38:14 -0600

    fix

--- entrypoint.sh
+++ entrypoint.sh
@@ -6,10 +6,6 @@ export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
 checkov -d $INPUT_WORKING_DIRECTORY $SKIP_CHECK_FLAG -o json > /tmp/tmp
 
-echo "----"
-cat /tmp/tmp
-echo "---"
-
 cat /tmp/tmp \
     | python3 /parse.py \
     | reviewdog -efm="%f:%l: %m" -name="checkov" -reporter="${INPUT_REPORTER}" -fail-on-error="${INPUT_FAIL_ON_ERROR}" -filter-mode="${INPUT_FILTER_MODE}"
