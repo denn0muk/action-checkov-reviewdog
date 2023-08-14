383f7560dc201c793785ccb1183cc58a3dedf407
Dennis Sserubiri <dsserubiri@tfin.com>
2023-08-13 19:28:31 -0500

    removed commented files

--- entrypoint.sh
+++ entrypoint.sh
@@ -5,26 +5,9 @@
 
 export REVIEWDOG_GITHUB_API_TOKEN="${INPUT_GITHUB_TOKEN}"
 
-
-# echo "checkov -d $INPUT_WORKING_DIRECTORY --download-external-modules false --quiet $SKIP_CHECK_FLAG -o json > /tmp/tmp"
-
-# checkov -d $INPUT_WORKING_DIRECTORY --download-external-modules $INPUT_DOWNLOAD_EXTERNAL_MODULES --quiet $SKIP_CHECK_FLAG -o json > /tmp/tmp
-
-# echo "=========================================="
-# ls -ltrh /tmp/tmp
-# cat /tmp/tmp
-# echo "=========================================="
-# cat /tmp/tmp \
-#     | python3 /parse.py
-# echo "=========================================="
-
 touch /tmp/output_file
 checkov -d $INPUT_WORKING_DIRECTORY $CHECK_BASELINE --download-external-modules $INPUT_DOWNLOAD_EXTERNAL_MODULES --quiet $SKIP_CHECK_FLAG -o json > /tmp/output_file
-# echo "=========================================="
-ls -ltrh /tmp/output_file
-cat /tmp/output_file
 
-# checkov -d $INPUT_WORKING_DIRECTORY $CHECK_BASELINE --download-external-modules $INPUT_DOWNLOAD_EXTERNAL_MODULES --quiet $SKIP_CHECK_FLAG -o json \
 cat /tmp/output_file \
     | python3 /parse.py \
     | reviewdog -efm="%f:%l: %m" -name="checkov" -reporter="${INPUT_REPORTER}" -fail-on-error="${INPUT_FAIL_ON_ERROR}" -filter-mode="${INPUT_FILTER_MODE}"
